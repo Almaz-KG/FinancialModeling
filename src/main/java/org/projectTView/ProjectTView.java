94b977c6eff0cc191b35e9117dfecb6df236ae3c
Almaz_KG <muktaalmaz@yandex.ru>
2014-09-03 21:29:50 +0400

    Created Test.java

--- src/main/java/org/projectTView/ProjectTView.java
+++ src/main/java/org/projectTView/ProjectTView.java
@@ -1,7 +1,6 @@
 package org.projectTView;
 
 import com.alee.laf.WebLookAndFeel;
-import com.alee.laf.rootpane.WebFrame;
 
 /**
  * Created by almu0214 on 03.09.2014.
@@ -15,10 +14,6 @@ public class ProjectTView {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
-
-
-
 
 
       //  WebLookAndFeel.setDecorateDialogs ( decorateFrames );
---
+++ src/test/Test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Almaz on 03.09.2014.
+ */
+public class Test {
+}
